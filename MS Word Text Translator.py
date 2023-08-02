4a9d0bea907e600823d4946a4c3c78c4c4ef7f94
AbieAnders <136343810+AbieAnders@users.noreply.github.com>
2023-08-02 09:58:17 +0530

    Updated MS Word Text Translator.py
    Added the fast translation code that doesn't work yet

--- MS Word Text Translator.py
+++ MS Word Text Translator.py
@@ -9,10 +9,13 @@ from docx.oxml.table import CT_Tbl
 #translatepy library
 from translatepy import Translator
 
-os.chdir('Path of the desired new working directory')
+os.chdir('C:/Users/Ab/VsCode Projects/Projects/Text Translator PSG')
+#Path of the desired new working directory
 doc = docx.Document()
 doc.save('Fully Translated Document.docx')
-doc1 = docx.Document('Document of choice.docx')
+doc1 = docx.Document('7. SLM Fundamentals of E-commerce.docx')
+#Document of choice.docx
+print("The documents were accessed successfully.")
 
 translator_object = Translator()
 def Translate_Text(file_text):
@@ -22,10 +25,11 @@ def Translate_Text(file_text):
             return None
         return str(translation)
     except IndexError:
+        #print("Index error")
         return None
-    except Exception as e:
+    '''except Exception as e:
         print("An error has occurred during translation:", e)
-        return None
+        return None'''
 
 def iter_block_items(parent):
     """
@@ -42,7 +46,7 @@ def iter_block_items(parent):
     elif isinstance(parent, _Row):
         parent_elm = parent._tr
     else:
-        raise ValueError("Something's not right")
+        raise ValueError("something's not right")
     for child in parent_elm.iterchildren():
         if isinstance(child, CT_P):
             yield Paragraph(child, parent_elm)
@@ -50,10 +54,14 @@ def iter_block_items(parent):
             yield Table(child, parent_elm)
 
 #execution loop
-for count, block in enumerate(iter_block_items(doc1)):
-    #print("block count:", count)
+para_list = []
+text_segment = ""
+
+'''for count, block in enumerate(iter_block_items(doc1)):
+    print("block count:", count)
     if isinstance(block, Paragraph):
         if block.text.strip() == '':
+            print(count)
             continue
         block = Translate_Text(block.text)
         doc.add_paragraph(block)
@@ -61,10 +69,43 @@ for count, block in enumerate(iter_block_items(doc1)):
     elif isinstance(block, Table):
         n = len(block.rows)
         m = int(len(block._cells)/n)
+        print(n, m)
         table = doc.add_table(n, m)
         for a,row in enumerate(block.rows):
             for b,cell in enumerate(row.cells):
                 translation = translator_object.translate(cell.text,'ta')
                 table.cell(a, b).text = str(translation)  #str type casting is necessary since the text function automatically assumes the TranslationResult type
-        #table.style = 'Colorful List'
-        doc.save('Fully Translated Document.docx')
+'''
+for count, block in enumerate(iter_block_items(doc1)):
+    print("block count:", count)
+    if isinstance(block, Paragraph):
+        try:
+            if block.text.strip() == '':
+                raise Exception('Whitespace')
+            print("##Exception is not being called.")
+            print(block.text, "###Has been appended")
+            para_list.append(block.text) #doesnt append after block 264(whole thing crumbles)
+        except Exception as e:
+            print("##Exception is being called.")
+            #print(para_list)
+            text_segment = '\n'.join(para_list)
+            data = Translate_Text(text_segment)
+            #data = text_segment
+            print(data)
+            doc.add_paragraph(data)
+            para_list = []
+            text_segment = ""
+            doc.save('Fully Translated Document.docx')
+            continue
+    elif isinstance(block, Table):
+        n = len(block.rows)
+        m = int(len(block._cells)/n)
+        print(n, m)
+        '''table = doc.add_table(n, m)
+        for a,row in enumerate(block.rows):
+            for b,cell in enumerate(row.cells):
+                translation = translator_object.translate(cell.text,'ta')
+                table.cell(a, b).text = str(translation)  #str type casting is necessary since the text function automatically assumes the TranslationResult type
+'''
+#table.style = 'Colorful List'
+doc.save('Fully Translated Document.docx')
